eeb7903b0a48f0b6273f8ca479d663fc8577643c
アミール　モハメド <mariemosalca@Amirs-MacBook.fios-router.home>
2023-11-14 07:31:44 -0500

    added About me page..

--- amir-joseph/src/components/Navbar/Navbar.js
+++ amir-joseph/src/components/Navbar/Navbar.js
@@ -19,23 +19,24 @@ const Navbar = () => {
     const handleNavLinkClick = (url) => {
       scrollToTop();
       navigate(url);
+      setActiveNav(url);
     };
 
   return (
     <nav>
-        <Link to='/' onClick={() => setActiveNav('/')}  className={activeNav === '/' ? 'active' : ''}>
+        <Link to='/' onClick={() => handleNavLinkClick('/')}  className={activeNav === '/' ? 'active' : ''}>
         <FontAwesomeIcon icon={faHouse} />
         </Link>
-        <Link to='/aboutme' onClick={() => setActiveNav('/aboutme')}  className={activeNav === '/aboutme' ? 'active' : ''}>
+        <Link to='/aboutme' onClick={() => handleNavLinkClick('/aboutme')}  className={activeNav === '/aboutme' ? 'active' : ''}>
         <FontAwesomeIcon icon={faUserCircle} />
         </Link>
-        <Link to='/experiences' onClick={() => setActiveNav('/experiences')}  className={activeNav === '/experiences' ? 'active' : ''}>
+        <Link to='/experiences' onClick={() => handleNavLinkClick('/experiences')}  className={activeNav === '/experiences' ? 'active' : ''}>
         <FontAwesomeIcon icon={faBook} />
         </Link>
-        <Link to='/services' onClick={() => setActiveNav('/services')}  className={activeNav === '/services' ? 'active' : ''}>
+        <Link to='/services' onClick={() => handleNavLinkClick('/services')}  className={activeNav === '/services' ? 'active' : ''}>
         <FontAwesomeIcon icon={faHandshake} />
         </Link>
-        <Link to='/contact' onClick={() => setActiveNav('/contact')}  className={activeNav === '/contact' ? 'active' : ''}>
+        <Link to='/contact' onClick={() => handleNavLinkClick('/contact')}  className={activeNav === '/contact' ? 'active' : ''}>
         <FontAwesomeIcon icon={faAddressBook} />
         </Link>
     </nav>
--- amir-joseph/src/pages/AboutMe.js
+++ amir-joseph/src/pages/AboutMe.js
@@ -1,9 +1,75 @@
 import React from 'react'
+import '../App.css'
+import '../index.css'
+import ME from '../components/assets/Images/picwitcoffee.PNG'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faUsers, faAward, faListCheck  } from '@fortawesome/free-solid-svg-icons';
+// import { Col, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
 
 const AboutMe = () => {
-  return (
-    <div>AboutMe</div>
-  )
+    return (
+        <div className="home">
+          <div className='mt-5 mb-5 pb-5'>
+                <h5>
+                    Get To Know
+                </h5>
+                <h2>
+                    About Me
+                </h2>
+            </div>
+            <div className='about__container pt-5'>   
+           
+                <div className='about__me'>
+                    <div className='about__me-image'>
+                        <img src={ME} alt='me' className='mt-5' />
+                    </div>
+                </div>
+
+
+
+                <div className='about__content'>
+                    <div className='about__cards'>
+                        <article className='about-card'>
+                            <FontAwesomeIcon icon={faAward} className='about-icon' />
+                            <h4 className='pl-0'>
+                                Experience
+                            </h4>
+                            <small>
+                                4+ Years of Work Experience
+                            </small>
+                        </article>
+                        <article className='about-card'>
+                            <FontAwesomeIcon icon={faUsers} className='about-icon' />
+                            <h4 className='pl-0'>
+                                Clients
+                            </h4>
+                            <small>
+                                +2 Very Satisfied Clients
+                            </small>
+                        </article>
+                        <article className='about-card'>
+                            <FontAwesomeIcon icon={faListCheck} className='about-icon' />
+                            <h4 className='pl-0'>
+                                Projects
+                            </h4>
+                            <small>
+                                30+ Completed Projects
+                            </small>
+                        </article>
+                    </div>
+
+
+                    <Link to='/contact'>
+                        <a href="#contact"className='btn btn-primary'>
+                            Lets Talk
+                        </a>
+                    </Link>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default AboutMe 
